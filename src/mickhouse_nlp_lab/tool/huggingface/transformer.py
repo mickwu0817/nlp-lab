db9ca1f73277fb182813e9d971ce33afb4525a91
26775 <>
2024-03-07 07:00:39 +0800

    Update transformer.py

--- src/mickhouse_nlp_lab/tool/huggingface/transformer.py
+++ src/mickhouse_nlp_lab/tool/huggingface/transformer.py
@@ -17,6 +17,48 @@ class Dataset(torch.utils.data.Dataset):
         return get_data(self.vocabulary['vocab_x'], self.vocabulary['vocab_y'])
 
 
+class MultiHead(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc_Q = torch.nn.Linear(32, 32)
+        self.fc_K = torch.nn.Linear(32, 32)
+        self.fc_V = torch.nn.Linear(32, 32)
+        self.out_fc = torch.nn.Linear(32, 32)
+        self.norm = torch.nn.LayerNorm(normalized_shape=32, elementwise_affine=True)
+        self.dropout = torch.nn.Dropout(p=0.1)
+
+    def forward(self, Q, K, V, mask):
+        # b句话,每句话50个词,每个词编码成32维向量
+        # Q、K、V = [b, 50, 32]
+        b = Q.shape[0]
+        # 保留下原始的Q,后面要做短接用
+        clone_Q = Q.clone()
+        # 标准化
+        Q = self.norm(Q)
+        K = self.norm(K)
+        V = self.norm(V)
+        # 线性运算,维度不变
+        # [b, 50, 32] -> [b, 50, 32]
+        K = self.fc_K(K)
+        V = self.fc_V(V)
+        Q = self.fc_Q(Q)
+        # 拆分成多个头
+        # b句话,每句话50个词,每个词编码成32维向量,4个头,每个头分到8维向量
+        # [b, 50, 32] -> [b, 4, 50, 8]
+        Q = Q.reshape(b, 50, 4, 8).permute(0, 2, 1, 3)
+        K = K.reshape(b, 50, 4, 8).permute(0, 2, 1, 3)
+        V = V.reshape(b, 50, 4, 8).permute(0, 2, 1, 3)
+        # 计算注意力
+        # [b, 4, 50, 8] -> [b, 50, 32]
+        score = attention(Q, K, V, mask)
+        # 计算输出,维度不变
+        # [b, 50, 32] -> [b, 50, 32]
+        score = self.dropout(self.out_fc(score))
+        # 短接
+        score = clone_Q + score
+        return score
+
+
 def get_vocabulary()->list[dict]:
     vocab_x = '<SOS>,<EOS>,<PAD>,0,1,2,3,4,5,6,7,8,9,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m'
     vocab_x = {word: i for i, word in enumerate(vocab_x.split(','))}
@@ -122,6 +164,28 @@ def mask_tril(data) -> Tensor:
     # 转布尔型,增加一个维度,便于后续的计算
     mask = (mask == 1).unsqueeze(dim=1)
     return mask
+
+
+def attention(Q, K, V, mask):
+    # b句话,每句话50个词,每个词编码成32维向量,4个头,每个头分到8维向量
+    # Q、K、V = [b, 4, 50, 8]
+    # [b, 4, 50, 8] * [b, 4, 8, 50] -> [b, 4, 50, 50]
+    # Q、K矩阵相乘,求每个词相对其他所有词的注意力
+    # permute 維度換位
+    score = torch.matmul(Q, K.permute(0, 1, 3, 2))
+    # 除以每个头维数的平方根,做数值缩放
+    score /= 8**0.5
+    # mask遮盖,mask是true的地方都被替换成-inf,这样在计算softmax的时候,-inf会被压缩到0
+    # mask = [b, 1, 50, 50]
+    score = score.masked_fill_(mask, -float('inf'))
+    score = torch.softmax(score, dim=-1)
+    # 以注意力分数乘以V,得到最终的注意力结果
+    # [b, 4, 50, 50] * [b, 4, 50, 8] -> [b, 4, 50, 8]
+    score = torch.matmul(score, V)
+    # 每个头计算的结果合一
+    # [b, 4, 50, 8] -> [b, 50, 32]
+    score = score.permute(0, 2, 1, 3).reshape(-1, 50, 32)
+    return score
 
 
 if __name__ == '__main__':
